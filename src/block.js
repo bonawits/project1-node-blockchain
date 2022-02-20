ee5b5f778eabe6c822346da48a630f03447a84e3
Craig Sloane <sloanecr@gmail.com>
2022-02-20 18:15:58 +0000

    code cleanup

--- src/block.js
+++ src/block.js
@@ -35,7 +35,6 @@ class Block {
    *  Note: to access the class values inside a Promise code you need to create an auxiliary value `let self = this;`
    */
   validate() {
-    console.log("VALIDATE BLOCK");
     let self = this;
     return new Promise((resolve, reject) => {
       // Save in auxiliary variable the current block hash
